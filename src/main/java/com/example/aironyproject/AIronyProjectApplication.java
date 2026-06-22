be86b0c6b05ccc8a8e87323c016f771e98d61603
kera9960 <kera9960@gmail.com>
2026-06-22 10:02:25 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/example/aironyproject/AIronyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.aironyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIronyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIronyProjectApplication.class, args);
+	}
+
+}
