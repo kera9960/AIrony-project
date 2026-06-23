ed50435eeda127b8c82c433e607a8994a6dbce47
kera9960 <kera9960@gmail.com>
2026-06-23 10:32:23 +0900

    chore: BaseEntity 및 전역 예외 패키지 생성

---
+++ src/main/java/com/example/aironyproject/common/entity/BaseTimeEntity.java
@@ -0,0 +1,5 @@
+package com.example.aironyproject.common.entity;
+
+public class BaseTimeEntity {
+
+}
---
+++ src/main/java/com/example/aironyproject/domain/payment/entity/Payment.java
@@ -0,0 +1,5 @@
+package com.example.aironyproject.domain.payment.entity;
+
+public class Payment {
+
+}
