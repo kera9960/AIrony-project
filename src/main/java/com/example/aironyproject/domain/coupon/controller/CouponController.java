254ad8c4fcfe4f0b2755a6545e4ee3f730fd690d
mavubsa3-dev <mavubsa3@gmail.com>
2026-06-24 20:04:43 +0900

    불필요한 import문 제거

--- src/main/java/com/example/aironyproject/domain/coupon/controller/CouponController.java
+++ src/main/java/com/example/aironyproject/domain/coupon/controller/CouponController.java
@@ -1,12 +1,9 @@
 package com.example.aironyproject.domain.coupon.controller;
 
 import java.util.List;
-
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/aironyproject/domain/userCoupon/service/UserCouponService.java
+++ src/main/java/com/example/aironyproject/domain/userCoupon/service/UserCouponService.java
@@ -1,19 +1,9 @@
 package com.example.aironyproject.domain.userCoupon.service;
 
-
-
-import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.example.aironyproject.common.exception.CustomException;
-import com.example.aironyproject.common.exception.ErrorCode;
-import com.example.aironyproject.domain.coupon.dto.GetCouponResponse;
-import com.example.aironyproject.domain.user.repository.UserRepository;
 import com.example.aironyproject.domain.userCoupon.dto.GetMyCouponResponse;
-import com.example.aironyproject.domain.userCoupon.entity.UserCoupon;
 import com.example.aironyproject.domain.userCoupon.repository.UserCouponRepository;
 
 import lombok.RequiredArgsConstructor;
